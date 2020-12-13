f0d9e650c9240962d18fb9240dcd4d9446f38a1f
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2020-12-13 15:54:19 -0600

    Added port 3000 as fallback

--- server.js
+++ server.js
@@ -33,12 +33,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-// // listen for requests :)
-// var listener = app.listen(3000, function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || '3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
