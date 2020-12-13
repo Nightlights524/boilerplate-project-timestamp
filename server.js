dae1c3b56e62d0740d5cda6595f0a965d75ab39e
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2020-12-13 15:09:15 -0600

    Fix port to 3000

--- server.js
+++ server.js
@@ -18,15 +18,17 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// // listen for requests :)
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
